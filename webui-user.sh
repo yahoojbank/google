47a31b5bd994a3aa418a0ff804be383ef60d14e9
yahoojbank <119779144+yahoojbank@users.noreply.github.com>
2023-07-15 14:35:04 +0700

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -7,7 +7,7 @@
 #install_dir="/home/$(whoami)"
 
 # Name of the subdirectory
-#clone_dir="stable-diffusion-webui"
+clone_dir="google"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
 #export COMMANDLINE_ARGS=""
